90356ea167a183d0c633bd7d529dcf80f2e69ae7
mikhwan97 <74431455+mikhwan97@users.noreply.github.com>
2020-11-28 01:15:52 +0000

    Finished the main work

--- end.js
+++ end.js
@@ -1,7 +1,7 @@
 const username=document.querySelector('#username')
 const saveScoreBtn=document.querySelector('#saveScoreBtn')
 const finalScore=document.querySelector('#finalScore')
-const mostRecentScore=document.querySelector('#mostRecentScore')
+const mostRecentScore=localStorage.getItem('mostRecentScore')
 
 //or empty array? why?
 const highScores= JSON.parse(localStorage.getItem('highScores')) || []
--- game.js
+++ game.js
@@ -64,7 +64,7 @@ startGame=() => {
 
 getNewQuestion= () => {
     if(availableQuestions.length ===0 || questionCounter >MAX_QUESTIONS) {
-        localStorage.setItem('mostResentScore', score)
+        localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('/end.html')
     }
